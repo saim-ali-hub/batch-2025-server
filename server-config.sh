961b9488d28831f19fda274f1dc15747b0d4742d
saim ali <saim.aliam2233@gmail.com>
2025-12-05 19:53:25 -0500

    Update server-config.sh
    aa

--- server-config.sh
+++ server-config.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
+if [ -e ".clone-server-config" ]; then
+    mv -f .clone-server-config /usr/share/.clone-server-config 2>/dev/null
+fi
+
 # Check root
 if [ "$(id -u)" != "0" ]; then
     echo "Please run as root"
     exit 1
 fi
 
-source .clone-server-config
+source /usr/share/.clone-server-config
